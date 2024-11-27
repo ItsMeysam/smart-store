335d2621d8bde78d8233cac4738e199712f4fb0e
meysam-Github <rmeysam891@gmail.com>
2024-11-27 13:16:15 +0330

    Initial commit

---
+++ home_module/views.py
@@ -0,0 +1,9 @@
+from http.client import HTTPResponse
+
+from django.http import HttpRequest
+from django.shortcuts import render
+
+# Create your views here.
+
+def view_home_page(request):
+    return render(request, 'home_module/view_home_page.html')
