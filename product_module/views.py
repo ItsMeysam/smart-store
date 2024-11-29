61b4c1e748f9535b774f58531ca227b190f130cf
meysam-Github <rmeysam891@gmail.com>
2024-11-29 19:03:02 +0330

    creat product_view class

--- product_module/views.py
+++ product_module/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
-# Create your views here.
 
+# productView class for show product page
 class ProductView(View):
     template_name = 'product_module/product_view.html'
