2e79c58d98a4edf84e3e3ce93050beef46487f7c
meysam <rmeysam891@gmail.com>
2025-02-08 20:42:46 +0330

    sort by price or -price

--- product_module/views.py
+++ product_module/views.py
@@ -11,9 +11,25 @@ class ProductListView(ListView):
     paginate_by = 4
 
 
+
+    def get_queryset(self):
+        """
+        این متد برای انجام مرتب‌سازی محصولات بر اساس قیمت است.
+        اگر پارامتر 'sort' در URL باشد، از آن استفاده می‌کند.
+        """
+        sort_order = self.request.GET.get('sort', 'asc')  # پیش‌فرض 'asc'
+        
+        if sort_order == 'asc':
+            return Product.objects.all().order_by('price')
+        elif sort_order == 'desc':
+            return Product.objects.all().order_by('-price')
+        else:
+            return Product.objects.all() 
+
 class ProductDetailView(DetailView):
     template_name = 'product_module/product_detail_view.html'
     model = Product
+
 
 
 
