320fa2e7576a45020fb82a09a57dc0f698adcbcc
meysam <rmeysam891@gmail.com>
2025-02-10 18:01:59 +0330

    new product feature

--- home_module/views.py
+++ home_module/views.py
@@ -11,23 +11,39 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        products:Product = Product.objects.all()
-        Product.objects.order_by('-created_at')[:5]
-        random_products = Product.objects.order_by('?')[:4]
+
+        # دریافت پارامتر 'sort' از URL (اگر وجود داشته باشد)
+        sort_order = self.request.GET.get('sort', 'asc')  # به طور پیش‌فرض صعودی است
 
 
+        # بخش دوم: جدیدترین محصولات (بر اساس تاریخ اضافه شدن)
+        latest_products = Product.objects.all().order_by('-created_at')[:4]
 
-        paginator = Paginator(products, 4)
+        # مرتب‌سازی محصولات بر اساس قیمت
+        if sort_order == 'desc':
+            products = Product.objects.all().order_by('-price')  # بالاترین قیمت به پایین
+        else:
+            products = Product.objects.all().order_by('price')  # پایین‌ترین قیمت به بالا
+
+        # محصولات تصادفی برای نمایش در بخش رندوم
+        random_products = Product.objects.all().order_by('?')[:4]
+
+        # صفحه‌بندی
+        paginator = Paginator(products, self.paginate_by)
         page_number = self.request.GET.get('page', 1)
 
         try:
             page_obj = paginator.get_page(page_number)
         except:
             raise Http404("صفحه مورد نظر موجود نیست.")
+
+        # اضافه کردن محصولات صفحه‌بندی شده و دیگر اطلاعات به context
         context['random_products'] = random_products
         context['page_obj'] = page_obj
         context['products'] = page_obj.object_list
         context['paginator'] = paginator
+        context['latest_products'] = latest_products 
+
         return context
 
 
